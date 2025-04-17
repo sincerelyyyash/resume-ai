69d7d538e8d840c98e7117f187f294e6cc135b28
sincerelyyyash <yta0088@gmail.com>
2025-04-17 23:23:35 +0530

    dashboard init

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import JobDescriptionForm from '@/components/forms/JobDescriptionForm';
+
+export default function DashboardPage() {
+  return (
+    <div className="h-full p-8">
+      <JobDescriptionForm />
+    </div>
+  );
+}
