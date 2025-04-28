4de78698463c3f33f533aedb52c02cfbc4bbb936
sincerelyyyash <yta0088@gmail.com>
2025-04-28 21:09:41 +0530

    appbar modified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         <Providers>
           <div className="flex flex-col min-h-screen">
             <header className="flex-none w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              {/* <Appbar /> */}
+              <Appbar />
             </header>
             <main className="flex-grow w-full max-w-7xl mx-auto px-4 sm:px-8 dark:bg-zinc-900">
               {children}
